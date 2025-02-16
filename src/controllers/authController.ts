49833f8c8a1adb4745b51a763c0e5ca78773e833
evya61992 <evya61992@gmail.com>
2025-02-16 16:46:09 +0200

    routes controllers and middleware implemented

--- src/controllers/authController.ts
+++ src/controllers/authController.ts
@@ -1,6 +1,6 @@
 import { Request, Response } from 'express';
 import jwt from 'jsonwebtoken';
-import bcrypt from 'bcryptjs';
+import bcrypt from 'bcrypt';
 import mongoose from 'mongoose';
 import { User, IUser } from '../models/userModel';
 
